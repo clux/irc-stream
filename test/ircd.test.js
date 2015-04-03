f4a150944765ae4f66e253b606898b2fe23840f6
clux <analsandblaster@gmail.com>
2015-04-03 09:33:54 +0100

    tests for writer

--- test/ircd.test.js
+++ test/ircd.test.js
@@ -10,20 +10,19 @@ function Server() {
   this.ircd.listen(6667);
   this.log = new Smell();
   this.bot = null;
+  this.person = null;
   var self = this;
   this.ircd.on('connection', function (c) {
     c.on('authenticated', function () {
       self.log.info(c.mask + " connected");
-      if (c.nickname === 'bot') {
-        self.bot = c;
-      }
+      self[c.nickname === 'bot' ? 'bot' : 'person'] = c;
     });
 
     c.on('PRIVMSG', function (target, message) {
-      // Ensure pms and chan messages in #test reach the bot:
-      if ((target === 'bot' || target === '#test') && self.bot) {
-        self.bot.send(':' + c.nickname + ' PRIVMSG ' + target + ' :' + message);
-      }
+      // bot sends to person, person sends to bot:
+      //self.log.info('server handle "' + message + '" for', target);
+      var dest = self[self.bot.id === c.id ? 'person' : 'bot'];
+      dest.send(':' + c.nickname + ' PRIVMSG ' + target + ' :' + message);
     });
 
     c.on('disconnected', function () {
@@ -42,8 +41,9 @@ Server.prototype.registerClient = function (bot) {
 
 
 
-exports.basic =  function (t) {
+exports.everythingOn =  function (t) {
   var ircOpts = { channel: [ '#test' ] };
+  //var ircOptsDebug = { channel: [ '#test' ], debug: true };
 
   // 1. set up an IRC server
   var server = new Server();
@@ -59,6 +59,13 @@ exports.basic =  function (t) {
 
   // 3. connect a normal IRC `person` to the server
   var person = new Client('localhost', 'dude', ircOpts);
+  person.addListener('error', function () {});
+
+  var cleanup = function () {
+    person.disconnect();
+    istream.bot.disconnect();
+    server.close();
+  };
 
   // 4. say something from `person` to `bot` when ready
   person.addListener('registered', function () {
@@ -75,16 +82,34 @@ exports.basic =  function (t) {
       t.deepEqual(m2, excp2, 'chan message read');
 
       var m3 = istream.read();
-      var excp3 = {"user":"#test:dude","name":"dude","message":"participate"};
+      var excp3 = { user: "#test:dude", name: "dude", message: "participate" };
       t.deepEqual(m3, excp3, 'unrelated participation message read');
 
       var m4 = istream.read();
       t.equal(m4, null, 'no fourth message');
 
-      person.disconnect();
-      istream.bot.disconnect();
-      server.close();
-      t.done();
+      // write responses
+      istream.write({ user: 'dude', message: 'resp' });
+      istream.write({ user: '#test:dude', message: 'really?'}); // should highlight
+      istream.write({ user: '#test:dude', message: 'that is insane'}); // should not
     }, 50);
-  }).addListener('error', function () {});
+  });
+
+  // 5. collect responses
+  var resps = [];
+  person.addListener('message', function (from, to, message) {
+    resps.push({ from: from, to: to, message: message});
+  });
+
+  // 6. verify responses
+  setTimeout(function () {
+    var r1 = { from: 'bot', to: 'dude', message: 'resp' };
+    var r2 = { from: 'bot', to: '#test', message: 'dude: really?'};
+    var r3 = { from: 'bot', to: '#test', message: 'that is insane'};
+    t.deepEqual(resps[0], r1, 'response 1 to person');
+    t.deepEqual(resps[1], r2, 'response 2 to person');
+    t.deepEqual(resps[2], r3, 'response 3 to person');
+    cleanup();
+    t.done();
+  }, 150);
 };
