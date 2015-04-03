66206ef6ee54e3c34e335e05c6053f2dba4eb4ca
clux <analsandblaster@gmail.com>
2015-04-03 09:34:03 +0100

    clean up handlers

--- irc-stream.js
+++ irc-stream.js
@@ -23,6 +23,14 @@ function IrcStream(server, name, ircOpts, opts) {
     this.bot.addListener('error', function () {}); // never usually care about errors
   }
 
+  // listen for pms if option set (can do that immediately)
+  if (this.opts.answerPms) {
+    this.bot.addListener('pm', function (nick, msg) {
+      var o = {user: nick, name: nick, message: msg};
+      self.log.info("Incoming PM: %j", o);
+      self.push(o);
+    });
+  }
 
   // start listening in channel for messages to ourselves
   // but only after we have registered so we know what name we have
@@ -34,38 +42,26 @@ function IrcStream(server, name, ircOpts, opts) {
       return;
     }
     // respond directly in channel - to anything matching addressedReg
-    self.bot.addListener('message', function (from, to, msg) {
-      if (to[0] !== '#') {
-        return; // ignore pms - handled separately
-      }
-      var content = '';
-      if (addressedReg.test(msg)) {
-        content = msg.match(addressedReg)[1].trim();
+    self.bot.addListener('message#', function (from, to, text) {
+      var msg = '';
+      if (addressedReg.test(text)) {
+        msg = text.match(addressedReg)[1].trim();
       }
       else if (trials.bernoulli(self.opts.participationChance || 0)) {
-        content = msg.trim();
+        msg = text.trim();
       }
-      if (content) {
-        var o = {user: to + ':' + from, name: from, message: content};
-        self.log.info("IrcStream departure Chan: %j", o);
+      if (msg) {
+        var o = {user: to + ':' + from, name: from, message: msg};
+        self.log.info("Incoming Chan: %j", o);
         self.push(o);
       }
     });
   });
-
-  // listen for pms if option set (can do that immediately)
-  if (this.opts.answerPms) {
-    this.bot.addListener('pm', function (nick, msg) {
-      var o = {user: nick, name: nick, message: msg};
-      this.log.info("IrcStream departure PM: %j", o);
-      this.push(o);
-    }.bind(this));
-  }
 }
 IrcStream.prototype = Object.create(Duplex.prototype);
 
 IrcStream.prototype._write = function (obj, enc, cb) {
-  this.log.info("IrcStream arrival: %j", obj);
+  this.log.info("Outgoing: %j", obj);
   if (obj.user == null || obj.message == null) {
     throw new Error("Improper object written to IrcStream");
   }
